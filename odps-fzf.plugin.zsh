e04e9e4b2efe71d198f74cbb092f6c9e1d0c34d1
Wenxuan <wenxuangm@gmail.com>
2017-08-11 18:57:06 +0800

    add size function

--- odps-fzf.plugin.zsh
+++ odps-fzf.plugin.zsh
@@ -9,35 +9,52 @@ function oupdate() {
 function otable() {
     ([ -f ~/.odps-tables ] || oupdate) && fzf < ~/.odps-tables
 }
-function opeek() {
-    [ $# -gt 0 ] && tb=$@ || tb=`otable`
-    [ $? -eq 0 ] && ocmd 'select * from {} limit 10' $tb
-}
-function odesc() {
-    [ $# -gt 0 ] && tb=$@ || tb=`otable`
-    [ $? -eq 0 ] && ocmd "desc {}" $tb
-}
-function ocount() {
-    [ $# -gt 0 ] && tb=$@ || tb=`otable`
-    [ $? -eq 0 ] && ocmd 'count {}' $tb
-}
-function ofields() {
-    [ $# -gt 0 ] && tb=$@ || tb=`otable`
-    [ $? -eq 0 ] && odesc $tb | sed -e '1,/Field/d' -e '/+/d' | tr '|' ' ' | trim | grep .
-}
 function opartition() {
     [ $# -gt 0 ] && tb=$@ || tb=`otable`
     [ $? -eq 0 ] && ocmd 'show partitions {}' $tb | sed "s/=\(.*\)/='\1'/g" | fzf
+}
+
+function otdesc() {
+    [ $# -gt 0 ] && tb=$@ || tb=`otable`
+    [ $? -eq 0 ] && ocmd "desc {}" $tb
 }
 function opdesc() {
     [ $# -gt 0 ] && tb=$@ || tb=`otable`
     [ $? -eq 0 ] && pt=`opartition $tb` \
         && ocmd "desc $tb partition({})" $pt
 }
+
+function otcount() {
+    [ $# -gt 0 ] && tb=$@ || tb=`otable`
+    [ $? -eq 0 ] && ocmd 'count {}' $tb
+}
 function opcount() {
     [ $# -gt 0 ] && tb=$@ || tb=`otable`
     [ $? -eq 0 ] && pt=`opartition $tb` \
         && ocmd "count $tb partition({})" $pt
+}
+
+function __extract_size() {
+    grep -Eo "Size: [0-9]+" | grep -Eo "[0-9]+" | numfmt --to=iec --suffix=B --round=nearest
+}
+
+function otsize() {
+    [ $# -gt 0 ] && tb=$@ || tb=`otable`
+    [ $? -eq 0 ] && otdesc $tb | __extract_size
+}
+function opsize() {
+    [ $# -gt 0 ] && tb=$@ || tb=`otable`
+    [ $? -eq 0 ] && pt=`opartition $tb` \
+        && ocmd "desc $tb partition({})" $pt | __extract_size
+}
+
+function otpeek() {
+    [ $# -gt 0 ] && tb=$@ || tb=`otable`
+    [ $? -eq 0 ] && ocmd 'select * from {} limit 10' $tb
+}
+function ofields() {
+    [ $# -gt 0 ] && tb=$@ || tb=`otable`
+    [ $? -eq 0 ] && otdesc $tb | sed -e '1,/Field/d' -e '/+/d' | tr '|' ' ' | trim | grep .
 }
 function odownload() {
     postfix_list=(.`date +%F` .`date +%F.%T` .`date +%s` .txt .data .dat .csv)
